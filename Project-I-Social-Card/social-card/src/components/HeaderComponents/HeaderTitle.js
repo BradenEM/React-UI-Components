ae2ad85b88d696f28df6a1f8f628676465afd618
Braden Mason <bradenemason@gmail.com>
2019-05-08 22:53:33 -0500

    Date now updates in Header Title

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,11 +1,28 @@
 import React from "react";
 import "./Header.css";
 
+var today = new Date();
+var month = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec"
+];
+var date = today.getDate() + " " + month[today.getMonth()];
+
 const HeaderTitle = () => {
   return (
     <div className="title-bar">
       <span className="user">Lambda School</span>
-      <span className="handle"> @LambdaSchool • 26 Jan</span>
+      <span className="handle"> @LambdaSchool • {date}</span>
     </div>
   );
 };
